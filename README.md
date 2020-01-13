3bcada43aedb9bceb903e168f54b066e00fbd1e7
Austin Starks <as2779@cornell.edu>
2020-01-13 08:52:39 -0500

    Add easy/hard buttons and scoring functionality

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Color_Game_Project
+# Color Game Project
 RGB color guessing game created to practice HTML, CSS, and JavaScript
+
+This project was inspired made after watching a similar example on the Web Developer's Bootcamp. I made this project to demonstrate my ability to code in basic HTML, CSS, and JavaScript. 
+
+In the game, 3-6 colors appear on the screen (depending on the difficulty) and the goal is to guess the correct RGB color displayed from the options. 
+
+One thing I implemented that wasn't in the orginal game is a score. The score multiplier increases the more colors you get correct in a row. If you get a color wrong, refresh the page, or reset the colors, the score returns to 0.
--- colorGame.js
+++ colorGame.js
@@ -1,11 +1,14 @@
 var colors = generateColorsArray(6);
-
 var heading = document.getElementById("heading");
 var squares = document.querySelectorAll(".square");
 var goalColor = pickRandomColor();
 var goalDisplay = document.getElementById("goalDisplay");
 var message = document.querySelector("#message");
 var resetButton = document.getElementById("newGame");
+
+var score = 0;
+var scoreMultiplier = 1.0;
+var scoreDisplay = document.getElementById("scoreDisplay");
 
 goalDisplay.textContent = goalColor;
 resetButton.addEventListener("click", function () {
@@ -22,6 +25,14 @@ resetButton.addEventListener("click", function () {
 
     // change the heading back to grey and the goal to the new goal
     heading.style.backgroundColor = "#333131";
+
+    // Update the score: if message.textContent has "right" in it keep score
+    // otherwise reset score
+    if (message.textContent.includes("Right")) {
+        updateScore(score, scoreMultiplier);
+    } else {
+        updateScore(0, 1.0);
+    }
 
     // change the "right" or "wrong" message back to "pick a color"
     message.textContent = "Pick a color "
@@ -44,14 +55,22 @@ for (var i = 0; i < squares.length; i++) {
             message.textContent = "Right! You're so good at this; great job! :)";
             changeColorsCorrect(clickedColor);
             resetButton.textContent = "Play again "
+            updateScore(score + (100 * scoreMultiplier), scoreMultiplier + 0.5);
         } // otherwise, tell the user they are wrong/try again, change the clicked color to the background color
         else {
             message.textContent = "Wrong! Think a little bit harder!";
             this.style.backgroundColor = "#333131";
+            updateScore(0, 1.0)
         }
 
 
     })
+}
+
+function updateScore(newScore, scoreMult) {
+    score = newScore;
+    scoreMultiplier = scoreMult;
+    scoreDisplay.textContent = score;
 }
 
 function changeColorsCorrect(color) {
@@ -83,5 +102,7 @@ function generateRandomColor() {
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
     var str = "rgb(" + r + ", " + g + ", " + b + ")";
+    // use below line to test game
+    // return "rgb(0, 255, 255)"
     return str;
 }
