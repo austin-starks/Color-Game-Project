809ade8f1ca812fb06916f3103cafc1b4cf4bbd3
Austin Starks <as2779@cornell.edu>
2020-01-13 12:47:01 -0500

    Refactor code into functions

--- colorGame.js
+++ colorGame.js
@@ -10,13 +10,105 @@ var score = 0;
 var scoreMultiplier = 1.0;
 var scoreDisplay = document.getElementById("scoreDisplay");
 
-goalDisplay.textContent = goalColor;
-resetButton.addEventListener("click", function () {
-    // pick 6 new colors
-    colors = generateColorsArray(6);
-    // assign the 6 new colors to the 6 squares
+var easyButton = document.querySelector("#easy")
+var hardButton = document.querySelector("#hard")
+var difficulty = 'hard'
+
+startGame();
+
+function startGame() {
+    easyButton.addEventListener("click", switchDifficulty)
+    hardButton.addEventListener("click", switchDifficulty)
+
+
+    goalDisplay.textContent = goalColor;
+    resetButton.addEventListener("click", function () {
+        if (difficulty === 'hard') {
+            resetGame(6);
+        } else {
+            resetGame(3);
+        }
+    })
+
+
+
     for (var i = 0; i < squares.length; i++) {
+        // add initial colors to squares
         squares[i].style.backgroundColor = colors[i];
+
+        // add click listener to squares
+        squares[i].addEventListener("click", addSquareListener)
+    }
+}
+
+function addSquareListener() {
+    var clickedColor = this.style.backgroundColor;
+    // if the clicked color is the same as the goal color, tell the user they are correct, change the rest of the colors to that color, 
+    // and change the heading to that color
+    if (clickedColor === goalColor) {
+        message.textContent = "Right! You're so good at this; great job! :)";
+        changeColorsCorrect(clickedColor);
+        resetButton.textContent = "Play again ";
+        if (difficulty === 'hard') {
+            var moreMult = 0.5;
+        } else {
+            var moreMult = 0.25;
+        }
+        updateScore(score + (100 * scoreMultiplier), scoreMultiplier + moreMult);
+    } // otherwise, tell the user they are wrong/try again, change the clicked color to the background color
+    else if (clickedColor !== goalColor) {
+        message.textContent = "Wrong! Think a little bit harder!";
+        this.style.backgroundColor = "#333131";
+        resetScore();
+    }
+}
+
+function switchDifficulty() {
+    // Change the difficulty
+
+    // change button disabled attributes depending on button press
+
+
+    // if difficulty switches to easy, must reset game with 3 colors
+
+    // otherwise, reset game with 6 colors
+
+    // if game goes to easy, reduce score gain for a win
+
+    // otherwise, return it to normal
+
+
+    if (difficulty === 'easy') {
+        difficulty = 'hard';
+        easyButton.disabled = false;
+        hardButton.disabled = true;
+        resetGame(6);
+    }
+    else {
+        difficulty = 'easy';
+        easyButton.disabled = true;
+        hardButton.disabled = false;
+        resetGame(3);
+    }
+    easyButton.classList.toggle("active");
+    hardButton.classList.toggle("active");
+}
+
+function resetGame(numSquares) {
+    // pick 6 new colors
+    colors = generateColorsArray(numSquares);
+    // assign the 6 new colors to the 6 squares
+    for (var i = 0; i < colors.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].addEventListener("click", addSquareListener);
+    }
+
+    // change the last elements of the array to be grey if colors.length is 3
+    if (colors.length != squares.length) {
+        for (var i = colors.length; i < squares.length; i++) {
+            squares[i].style.backgroundColor = "#333131";
+            squares[i].removeEventListener("click", addSquareListener);
+        }
     }
 
     // change the goal to color to a random color
@@ -31,40 +123,15 @@ resetButton.addEventListener("click", function () {
     if (message.textContent.includes("Right")) {
         updateScore(score, scoreMultiplier);
     } else {
-        updateScore(0, 1.0);
+        resetScore()
     }
 
     // change the "right" or "wrong" message back to "pick a color"
-    message.textContent = "Pick a color "
+    message.textContent = "Pick a color ";
 
     // change the text content to "New Colors "
     resetButton.textContent = "New Colors ";
     goalDisplay.textContent = goalColor;
-})
-
-for (var i = 0; i < squares.length; i++) {
-    // add initial colors to squares
-    squares[i].style.backgroundColor = colors[i];
-
-    // add click listener to squares
-    squares[i].addEventListener("click", function () {
-        var clickedColor = this.style.backgroundColor;
-        // if the clicked color is the same as the goal color, tell the user they are correct, change the rest of the colors to that color, 
-        // and change the heading to that color
-        if (clickedColor === goalColor) {
-            message.textContent = "Right! You're so good at this; great job! :)";
-            changeColorsCorrect(clickedColor);
-            resetButton.textContent = "Play again "
-            updateScore(score + (100 * scoreMultiplier), scoreMultiplier + 0.5);
-        } // otherwise, tell the user they are wrong/try again, change the clicked color to the background color
-        else {
-            message.textContent = "Wrong! Think a little bit harder!";
-            this.style.backgroundColor = "#333131";
-            updateScore(0, 1.0)
-        }
-
-
-    })
 }
 
 function updateScore(newScore, scoreMult) {
@@ -73,8 +140,16 @@ function updateScore(newScore, scoreMult) {
     scoreDisplay.textContent = score;
 }
 
+function resetScore() {
+    if (difficulty === 'easy') {
+        updateScore(0, 0.25);
+    } else {
+        updateScore(0, 1.0);
+    }
+}
+
 function changeColorsCorrect(color) {
-    for (var i = 0; i < squares.length; i++) {
+    for (var i = 0; i < colors.length; i++) {
         squares[i].style.backgroundColor = color;
     }
     heading.style.backgroundColor = color;
